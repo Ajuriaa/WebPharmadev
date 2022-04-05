a521d3546f303338a113335be414c4e378e7247f
Alejandro Ajuria <alejoajuria@gmail.com>
2022-04-04 18:43:07 -0600

    .l.

--- src/App.js
+++ src/App.js
@@ -9,18 +9,6 @@ import {store, persistor} from './Store';
 import Splash from './Components/Views/Splash';
 import Signin from './Components/Views/Signin/SigninPage';
 import Login from './Components/Views/Login/LoginPage';
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 39fa2782763bd61b2d8c17ad151d79b28dbde04e
-=======
-import Todo from './Components/Views/Todo/TodoPage';
->>>>>>> parent of 73d82b6 (Nada)
-=======
-
->>>>>>> parent of d3ed685 (Merge branch 'main' of https://github.com/Ajuriaa/WebPharmadev)
 import Productos from './Components/Views/Productos/ProductosPage';
 
 function App() {
@@ -32,7 +20,6 @@ function App() {
             <Route path="/" element={<Splash />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signin" element={<Signin />} />
-            <Route path="/todos" element={<Todo />} />
             <Route path="/productos" element={<Productos />} />
           </Routes>
         </BrowserRouter>
