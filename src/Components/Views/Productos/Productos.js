8dfc8561ad8873267bd7d6a3e9269eb821d82b18
damendoza5 <damendoza97@gmail.com>
2022-04-05 00:21:40 -0600

    Mapeo de productos

--- src/Components/Views/Productos/Productos.js
+++ src/Components/Views/Productos/Productos.js
@@ -1,14 +1,25 @@
 import Nav from "../../UX/Nav/Nav";
 import Page from "../../UX/Page/Page";
-import {Link} from "react-router-dom"
-
 
 const Productos = ({productos}) =>{
   return (
     <Page header={<h2>Productos</h2>} footer={<Nav/>}>
+      {
+        productos.map(({ _id,productoNombre,productoDescripcion }) => (
+          <ProductosItem key={_id} productoNombre={productoNombre} productoDescripcion={productoDescripcion} />
+        ))
+      }
     </Page>
   );
 }
 
+ const ProductosItem = ({productoNombre, productoDescripcion}) => {
+   return (
+     <section>
+       {productoNombre}
+       {productoDescripcion}
+     </section>
+   );
+ }
 
 export default Productos;
--- src/Components/Views/Productos/ProductosPage.js
+++ src/Components/Views/Productos/ProductosPage.js
@@ -11,9 +11,9 @@ const ProductosPage = ()=>{
       const loadData = async ()=> {
         dispatch({type:'PRODUCTOS_LOADING', payload:{}});
         try {
-          const { data: {Productos,  status}} = await privateAxios.get('/api/v1/productos/facet/1/10');
+          const { data: {productos,  status}} = await privateAxios.get('/api/v1/productos/all');
           dispatch({type:'PRODUCTOS_SUCCESS', payload: {productos}});
-          console.log(Productos);
+          console.log(productos);
         } catch(ex){
           console.log(ex);
           dispatch({ type: 'PRODUCTOS_FAILED', payload: {} });
@@ -26,10 +26,11 @@ const ProductosPage = ()=>{
     ,[]
   );
   const { productos, isLoading, errors } = useSelector(state=>state.productos);
+  console.log(productos)
   return (
     <>
       {isLoading && (<Loading />)}
-      <Productos/>
+      <Productos productos={productos}/>
     </>
   )
 }
